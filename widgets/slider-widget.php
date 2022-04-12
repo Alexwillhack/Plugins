56e35fef9dddd00c885f88e1f01c4aa039779849
Alexwillhack <z9110858@gmail.com>
2022-04-12 23:17:00 +0700

    Added Media

--- widgets/slider-widget.php
+++ widgets/slider-widget.php
@@ -40,14 +40,72 @@ class Elementor_Slider_Widget extends \Elementor\Widget_Base {
 		);
 
 		$this->add_control(
-			'url',
+			'image',
 			[
-				'label' => esc_html__( 'URL to embed', 'qwetheme-widget' ),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'input_type' => 'url',
-				'placeholder' => esc_html__( 'https://your-link.com', 'qwetheme-widget' ),
+				'label' => esc_html__( 'Choose Image', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+				'default' => [
+					'url' => \Elementor\Utils::get_placeholder_image_src(),
+				],
 			]
 		);
+
+
+
+		$this->add_control(
+			'list',
+			[
+				'label' => esc_html__( 'List', 'plugin-name' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => [
+					[
+						'name' => 'text',
+						'label' => esc_html__( 'Text', 'plugin-name' ),
+						'type' => \Elementor\Controls_Manager::TEXT,
+						'placeholder' => esc_html__( 'List Item', 'plugin-name' ),
+						'default' => esc_html__( 'List Item', 'plugin-name' ),
+					],
+					[
+						'name' => 'link',
+						'label' => esc_html__( 'Link', 'plugin-name' ),
+						'type' => \Elementor\Controls_Manager::URL,
+						'placeholder' => esc_html__( 'https://your-link.com', 'plugin-name' ),
+					],
+				],
+				'default' => [
+					[
+						'text' => esc_html__( 'List Item #1', 'plugin-name' ),
+						'link' => 'https://elementor.com/',
+					],
+					[
+						'text' => esc_html__( 'List Item #2', 'plugin-name' ),
+						'link' => 'https://elementor.com/',
+					],
+				],
+				'title_field' => '{{{ text }}}',
+			]
+		);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		$this->end_controls_section();
 
